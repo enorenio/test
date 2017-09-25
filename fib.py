a726c6f42fd21009c1d56703210bf84e76e0acda
enorenio <morshnev.aleksey@gmail.com>
2017-09-26 01:18:23 +0500

    +fibonacci numbers algorithms done

---
+++ fib.py
@@ -0,0 +1,27 @@
+#f = [1,1,1,1,1,1]
+def fib_recursive(n):
+	if n<2:
+		return 1
+	else:
+		return fib(n-1) + fib(n-2)
+
+def fib_classic(n):
+	f = [1 for x in range(n)]
+	for i in range(2,n):
+		f[i] = f[i-1] + f[i-2]
+	print(f[i])
+
+def fib_py(n):
+	a = 0
+	b = 1
+	for __ in range(n):
+		a, b = b, a + b
+	return a
+
+
+
+# realization
+
+n = int(input())
+print(fib_py(n))
+#fib(n)
