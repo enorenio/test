04900b4694023f8cc6ca764fd4176faf65ddb8be
enorenio <morshnev.aleksey@gmail.com>
2017-10-21 21:53:44 +0500

    MySQL now works!

--- dsm/alt/ss.py
+++ dsm/alt/ss.py
@@ -3,7 +3,7 @@ import json
 import configparser
 import pymysql
 
-connection=pymysql.connect(host="127.0.0.1", port=3306, user="Renion", passwd="password", db="recvdata")
+connection=pymysql.connect(host="127.0.0.1", port=3306, user="Renion", passwd="password", db="recvdata", autocommit=True)
 cursor = connection.cursor()
 
 Config = configparser.ConfigParser()
@@ -23,17 +23,32 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024)
         self.j = json.loads(self.data.decode())
-        print (int(self.j['one']) + int(self.j['two']) + int(self.j['three']))
-        print("{} wrote:".format(self.client_address[0]))
-        for item in self.j:
-            print(item, self.j[item])
 
-            try:
-                cursor.execute("INSERT INTO datatable ("+str(item)+") VALUES ("+str(self.j[item])+")")
-            except:
-                cursor.execute("ALTER TABLE datatable ADD "+str(item)+" INT DEFAULT "+str(self.j[item]))
-            finally:
-                sql_data = cursor.fetchall ()
+        #print (int(self.j['one']) + int(self.j['two']) + int(self.j['three']))
+
+        print("{} wrote:".format(self.client_address[0]))
+        #for item in self.j:
+        print(self.j["Date"])
+        print(self.j["IMEI"])
+        print(self.j["GEO"]['x'])
+        print(self.j["GEO"]['y'])
+        #cursor.execute("INSERT INTO datatable (Date, IMEI, GEO) VALUES ("+"'2012-01-01 14:22:13'"+", "+"123451234512345"+", "+"PointFromText(CONCAT('POINT(',1.25,' ',2.35,')'))"+");")
+        #UPDATE `datatable` SET `Date`='2012-01-01 14:22:13',`IMEI`=123451234512345,`GEO`=POINT(1,0) WHERE 1
+        # MySQL area (all with mistakes)
+        try:
+            print("--------")
+            print(self.j["Date"])
+            print("--------")
+            cursor.execute("INSERT INTO datatable (Date, IMEI, GEO) VALUES ('"+self.j["Date"]+"', "+self.j["IMEI"]+", "+"PointFromText(CONCAT('POINT(',"+self.j["GEO"]['x']+",' ',"+self.j["GEO"]['y']+",')'))"+");")
+            print("try error")
+        except:
+            print("except error")
+        finally:
+            print("finally")
+            sql_data = cursor.fetchall ()
+        # Mysql area end
+
+
 
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
